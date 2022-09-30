95a2ff701a9f31298a9a52552581b0fb73761251
Baigorama <82623042+BernaBaigo@users.noreply.github.com>
2022-09-30 15:54:18 -0300

    Add files via upload

---
+++ practico1/ejercicio6/index.php
@@ -0,0 +1,17 @@
+<?php require './layout/doctype.php' ?>
+
+    <h1>Tabla de multiplicar</h1>
+    <p>Ingrese el límite de la tabla</p>
+    <form action="calcularTabla.php" method="GET">
+        <p> 
+            <input type="number" name="fila" placeholder="Fila">
+        </p>
+        <p>
+            <input type="number" name="columna" id="" placeholder="Columna">
+        </p>
+    
+        <button type="submit">Multiplicar</button>
+    </form>
+    </body>
+
+<?php require './layout/end_doctype.php' ?>
